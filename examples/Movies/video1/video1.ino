28e930d1f465f4a353d7bc873635a8c42a827fd7
FT81x Manía <ft81xmania@gmail.com>
2018-06-07 00:06:41 +0200

    Update video1.ino

--- examples/Movies/video1/video1.ino
+++ examples/Movies/video1/video1.ino
@@ -8,13 +8,12 @@ void setup()
 void loop()
 {
   MoviePlayer mp;
-
   int randNumber = random(300);
   int fileNumber = randNumber % 5;
-  if (fileNumber == 0) mp.begin("fun-1500.avi");
-  if (fileNumber == 1) mp.begin("dj-1500.avi");
-  if (fileNumber == 2) mp.begin("spa-1500.avi");
-  if (fileNumber == 3) mp.begin("tra-1500.avi");
-  if (fileNumber == 4) mp.begin("tub-1500.avi");
+  if (fileNumber == 0) mp.begin("hands.avi");
+  if (fileNumber == 1) mp.begin("niki.avi");
+  if (fileNumber == 2) mp.begin("utopia.avi");
+  if (fileNumber == 3) mp.begin("sultans.avi");
+  if (fileNumber == 4) mp.begin("gost.avi");
   mp.play();
 }
