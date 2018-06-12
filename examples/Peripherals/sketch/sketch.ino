a4d37edffbee611dd9a2cbf568383f63535420f9
FT81x Manía <ft81xmania@gmail.com>
2018-06-12 17:57:41 +0200

    Add files via upload

---
+++ examples/Peripherals/sketch/sketch.ino
@@ -0,0 +1,23 @@
+#include <GD23ZU.h>
+
+void setup()
+{
+  delay(1000);Serial.begin(9600);
+  GD.begin();
+  GD.cmd_memset(0, 0, long(GD.w) * GD.h);   // clear the bitmap
+  GD.Clear();                           // draw the bitmap
+
+  GD.ColorRGB(0x202030);
+  GD.cmd_text(240, 136, 31, OPT_CENTER, "sketch demo");
+
+  GD.BitmapLayout(L8, GD.w, GD.h);
+  GD.BitmapSize(NEAREST, BORDER, BORDER, GD.w, GD.h);
+  GD.Begin(BITMAPS);
+  GD.ColorRGB(0xffffff);
+  GD.Vertex2ii(0, 0);
+  GD.swap();
+  GD.cmd_sketch(0, 0, GD.w, GD.h, 0, L8); // start sketching
+  GD.finish();                          // flush all commands
+}
+
+void loop() { }
